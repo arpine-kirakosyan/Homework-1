b41098d7014e4be850cae38a434bc0c80f172d6d
arpine-kirakosyan <67287945+arpine-kirakosyan@users.noreply.github.com>
2024-03-03 10:12:45 +0400

    Update TodoContext.js

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,4 +1,4 @@
-import React,  {useContext, useState, useEffect, createContext} from "react";
+import React,  {useContext, useState, createContext} from "react";
 
 
 export const TodoContext = createContext(null);
